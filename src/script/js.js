0e54044cbf78d9e39da2be5ba540efd7e4ca94e3
Denis Gorbunov <Gorbunosii@gmail.com>
2024-02-23 01:28:41 +0300

    ready

--- src/script/js.js
+++ src/script/js.js
@@ -128,5 +128,22 @@ $(`.acc__list`).accordion({
   heightStyle: `content`,
 });
 
+const toggleIcon = document.querySelector('.header__menu');
+const toggleSVG = document.querySelector('.header__menu_svg');
+const toggleP = document.querySelector('.header__menu_svg');
+
+console.log(toggleIcon);
+
+toggleIcon.addEventListener(`click`, e => {
+  const target = e.target;
+  if (target === toggleIcon || toggleSVG || toggleP) {
+    toggleSVG.classList.toggle('header__menu_svg-close');
+  }
+});
+
+$('.header__menu').click(() => {
+  $('.header__menu-list').slideToggle(300);
+});
+
 timerWork(timer.dataset.deadline);
 
